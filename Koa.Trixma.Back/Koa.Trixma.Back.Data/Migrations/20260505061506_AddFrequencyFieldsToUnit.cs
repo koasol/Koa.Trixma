d4aaa1dadf6a5cfefa00e19342b950ac5d1dbd44
Karsten <karsten.rennaes@gmail.com>
2026-05-05 08:19:59 +0200

    Update migrations

--- Koa.Trixma.Back/Koa.Trixma.Back.Data/Migrations/20260505061506_AddFrequencyFieldsToUnit.cs
+++ Koa.Trixma.Back/Koa.Trixma.Back.Data/Migrations/20260505061506_AddFrequencyFieldsToUnit.cs
@@ -8,12 +8,28 @@ namespace Koa.Trixma.Back.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.AddColumn<int>(
+                name: "PayloadIntervalS",
+                table: "Units",
+                type: "integer",
+                nullable: true);
 
+            migrationBuilder.AddColumn<int>(
+                name: "GnssRequestIntervalS",
+                table: "Units",
+                type: "integer",
+                nullable: true);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropColumn(
+                name: "PayloadIntervalS",
+                table: "Units");
 
+            migrationBuilder.DropColumn(
+                name: "GnssRequestIntervalS",
+                table: "Units");
         }
     }
 }
---
+++ Koa.Trixma.Back/add_columns.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "Units" ADD COLUMN IF NOT EXISTS "PayloadIntervalS" integer NULL;
+ALTER TABLE "Units" ADD COLUMN IF NOT EXISTS "GnssRequestIntervalS" integer NULL;

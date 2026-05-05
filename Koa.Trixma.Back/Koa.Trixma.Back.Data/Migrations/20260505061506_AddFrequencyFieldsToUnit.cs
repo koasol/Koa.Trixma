8b99b06db7ef74f2e07fbdd8eee78d5c95c818b9
Karsten <karsten.rennaes@gmail.com>
2026-05-05 08:16:56 +0200

    Update migrations

---
+++ Koa.Trixma.Back/Koa.Trixma.Back.Data/Migrations/20260505061506_AddFrequencyFieldsToUnit.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Koa.Trixma.Back.Data.Migrations
+{
+    public partial class AddFrequencyFieldsToUnit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
